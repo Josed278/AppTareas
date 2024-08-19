0979e4a4d4d176918f771a423a90976abfa5558b
JoseD <josed278@gmail.com>
2024-08-19 18:46:58 -0300

    creando componentes vistas

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,31 @@
-import React, { useState } from 'react';
-import EliminarAdjunto from './adjuntos/eliminar_adjunto';
-import ListadoProyecto from './proyectos/listado_proyecto';
-import NuevoProyecto from './proyectos/nuevo_proyecto';
-import DetalleProyecto from './proyectos/detalle_proyecto';
-import ActualizarProyecto from './proyectos/actualizar_proyecto';
-import ActualizarParcProyecto from './proyectos/actualizar_parc_proyecto';
-import EliminarProyecto from './proyectos/eliminar_proyecto';
+import { useState } from "react";
+import Menu from "./components/menu"; // Verifica la ruta de importación
+import Login from "./components/Login"; // Verifica la ruta de importación
+import Card from "./components/Card"; // Verifica la ruta de importación
+import Text from "./components/Text"; // Verifica la ruta de importación
+import Perfil from "./components/Perfil"; // Verifica la ruta de importación
+import NewCard from "./components/NewCard"; // Verifica la ruta de importación
+import NewProyectos from "./components/NewProyectos"; // Verifica la ruta de importación
+import Imagen from "./components/Imagen"; // Verifica la ruta de importación
 
-const App = () => {
-  const [adjuntoId, setAdjuntoId] = useState(1);
-
-  const handleDeleteSuccess = () => {
-    console.log('Adjunto eliminado exitosamente');
-    // Aquí puedes actualizar el estado o realizar otras acciones necesarias
-  };
-
+function App() {
   return (
     <div>
-      <EliminarAdjunto id={adjuntoId} onDeleteSuccess={handleDeleteSuccess} />
-      <ListadoProyecto />
-      <NuevoProyecto onCreateSuccess={handleCreateSuccess} />
-
-      <h1>Aplicación de Gestión de Proyectos</h1>
-      <DetalleProyecto id={proyectoId} />
-      <h1>Aplicación de Gestión de Proyectos</h1>
-      <ActualizarProyecto id={proyectoId} />
-      <h1>Aplicación de Gestión de Proyectos</h1>
-      <ActualizarParcProyecto id={proyectoId} />
-
-      <Route path="/eliminar-proyecto/:id" component={EliminarProyecto} />
-    
+      <Menu />
+      <Card />
+      <Perfil />
+      <NewCard />
+      <NewProyectos />
     </div>
-   
   );
-};
+}
 
 export default App;
+
+{
+  /* <Login />
+<Card />
+<Perfil />
+<NewCard />
+<NewProyectos /> */
+}
